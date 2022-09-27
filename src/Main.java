02f2d62789b13899a08dd4a92bb13f8042ba36b2
markd <dsilvamj@mail.uc.edu>
2022-09-26 22:24:33 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int intemPrice = 30;
+        double salesTax = intemPrice * 0.05;
+
+        System.out.println("Your sales tax will be: "+ salesTax + "$");
+    }
+}
